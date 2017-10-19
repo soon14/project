5fe6e6e042f5244ea064bb9da96f05ab91e1040d
tanyichao <tanyichao110@163.com>
2017-10-19 17:27:28 +0800

    clear pwd

--- data/common.inc.php
+++ data/common.inc.php
@@ -1,9 +1,9 @@
 <?php
 //数据库连接信息
-$cfg_dbhost = '127.0.0.1:3306';
-$cfg_dbname = 'stourwebcms';
-$cfg_dbuser = 'root';
-$cfg_dbpwd = 'mysql,2017,lvyou';
-$cfg_dbprefix = 'sline_';
+$cfg_dbhost = '';
+$cfg_dbname = '';
+$cfg_dbuser = '';
+$cfg_dbpwd = '';
+$cfg_dbprefix = '';
 $cfg_db_language = 'utf8';
 ?>
